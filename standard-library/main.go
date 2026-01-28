df86a73922fc1a7ff30913b6ae765cd8e60ced60
Danil Chugaev <dmchugaev@gmail.com>
2026-01-28 20:55:16 +0700

    add new handlers

--- standard-library/main.go
+++ standard-library/main.go
@@ -1,17 +1,56 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
 
-type homeHandler struct{}
+var (
+	RecipeRe       = regexp.MustCompile(`^/recipes/*$`)
+	RecipeReWithID = regexp.MustCompile(`^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
+)
 
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+type HomeHandler struct{}
+type RecipesHandler struct{}
+
+func (h *HomeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("This is my home page"))
+}
+
+func (h *RecipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) ListRecipe(w http.ResponseWriter, r *http.Request)   {}
+func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request)    {}
+func (h *RecipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
+
+func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodPost && RecipeRe.MatchString(r.URL.Path):
+		h.CreateRecipe(w, r)
+		return
+	case r.Method == http.MethodGet && RecipeRe.MatchString(r.URL.Path):
+		h.ListRecipe(w, r)
+		return
+	case r.Method == http.MethodGet && RecipeReWithID.MatchString(r.URL.Path):
+		h.GetRecipe(w, r)
+		return
+	case r.Method == http.MethodPut && RecipeReWithID.MatchString(r.URL.Path):
+		h.UpdateRecipe(w, r)
+		return
+	case r.Method == http.MethodDelete && RecipeReWithID.MatchString(r.URL.Path):
+		h.DeleteRecipe(w, r)
+		return
+	default:
+		return
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", &homeHandler{})
+	mux.Handle("/", &HomeHandler{})
+	mux.Handle("/recipes", &RecipesHandler{})
+	mux.Handle("/recipes/", &RecipesHandler{})
 
 	http.ListenAndServe(":8080", mux)
 }
